5e07b8ece086f76829c461e65c751a4726d57936
mazczc <87382876+mazczc@users.noreply.github.com>
2022-09-05 21:23:03 +0800

    Add files via upload

--- TB_Core.v
+++ TB_Core.v
@@ -24,7 +24,7 @@ module TB_Core();
     reg clk;
     reg rst_n;
     reg en;
-    reg [10: 0] addr;
+    reg [11: 0] addr;
     wire OUTPUT;
     integer i;
     Core C(clk, rst_n, en, addr, OUTPUT);
@@ -33,18 +33,18 @@ module TB_Core();
         clk = 0;
         rst_n = 1;
         en <= 1;
-        addr <= 11'b00000000000;
+        addr <= 11'b000000000000;
         
         #1 rst_n = 0;
         
         #1179662 en<=0;
         
-        #10 addr <= 11'b00000000001;
+        #10 addr <= 11'b000000000001;
         
-        #20 addr <= 11'b00000000011;
+        #20 addr <= 11'b000000000011;
         
         for ( i = 0; i < 1024; i = i + 1 ) begin
-            addr[11: 2] <= addr[11: 2] + 1;
+            #4 addr[11: 2] <= addr[11: 2] + 1;
         end
      end
     
